e432c133f421c32983bb486cc82f74b26454e741
axtlos <axtlos@tar.black>
2022-05-06 14:33:41 +0200

    test process output and fix it

--- lib/src/widgets/process_output.dart
+++ lib/src/widgets/process_output.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 
+//ignore: must_be_immutable
 class ProcessOutput extends StatelessWidget {
-    const ProcessOutput ({
-        Key? key,
-        required this.input,
-    }) : super(key: key);
+  ProcessOutput({
+    Key? key,
+    required this.input,
+  }) : super(key: key);
 
-    String input = "";
-    
-    @override
-    Widget build(BuildContext context) {
-        return Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-	            borderRadius: BorderRadius.circular(10),
-	            border: Border.all(color: Colors.black),
-	            color: Color.fromARGB(255, 15, 15, 15),
-            ),
-            child: SingleChildScrollView(
-                reverse: true,
-                child: Text(
-                input,
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontFamily: 'Monospace',
-                    fontWeight: FontWeight.w100,
-                ),
-            ),
+  String input = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.black),
+        color: const Color.fromARGB(255, 15, 15, 15),
+      ),
+      child: SingleChildScrollView(
+        reverse: true,
+        child: Text(
+          input,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontFamily: 'Monospace',
+            fontWeight: FontWeight.w100,
           ),
-        );
-    }
+        ),
+      ),
+    );
+  }
 }
